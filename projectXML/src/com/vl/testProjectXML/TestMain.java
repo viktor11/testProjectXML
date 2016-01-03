89d738813f8617d48d11cd56e55959ca12e249a9
Viktor Lukovic <viktor_lukovic@hotmail.com>
2016-01-03 18:39:51 +0100

    New Print statements

--- projectXML/src/com/vl/testProjectXML/TestMain.java
+++ projectXML/src/com/vl/testProjectXML/TestMain.java
@@ -3,5 +3,8 @@ package com.vl.testProjectXML;
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("HEllo xml");
+		System.out.println("HEllo xml");
+		System.out.println("HEllo xml");
+		System.out.println("HEllo xml");
 	}
 }
